72431d29e821a4e5b7025261a182e730285abd9a
Piyush Tiwari <piyushiitk24@gmail.com>
2025-08-09 16:25:58 +0530

    refactor: remove unused user retrieval methods from User model
    Signed-off-by: Piyush Tiwari <piyushiitk24@gmail.com>

--- server/models/users.js
+++ server/models/users.js
@@ -17,46 +17,6 @@ const User = {
     const newUser = { username, createdAt: new Date() };
     await usersCollection.insertOne(newUser);
     return newUser;
-  },
-
-  static async findByUsername(username) {
-    const db = getDb();
-    const type = database.getType();
-
-    if (type === 'mongodb') {
-      const user = await db.collection('users').findOne({ username });
-      return user ? { id: user._id, username: user.username } : null;
-    } else {
-      return new Promise((resolve, reject) => {
-        db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(row || null);
-          }
-        });
-      });
-    }
-  },
-
-  static async findAll() {
-    const db = getDb();
-    const type = database.getType();
-
-    if (type === 'mongodb') {
-      const users = await db.collection('users').find({}).toArray();
-      return users.map(u => ({ id: u._id, username: u.username }));
-    } else {
-      return new Promise((resolve, reject) => {
-        db.all('SELECT * FROM users ORDER BY username', [], (err, rows) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(rows);
-          }
-        });
-      });
-    }
   }
 };
 
